1ecee267607f273da392054f7d60b4ffccd6961a
ddk16a <ddk16a@acu.edu>
2019-11-25 20:01:07 -0600

    think

--- server/socket.js
+++ server/socket.js
@@ -18,23 +18,6 @@ io.on('connect', (socket) => {
 	socket.on('paired', () => socket.broadcast.to(socket.room).emit('paired'));
 
 	socket.on('move', (piece, dest) => socket.broadcast.to(socket.room).emit('updateBoard', piece, dest));
-	
-	// socket.on('lose', (color) => socket.to(socket.room).emit("gameover", color));
-	
-	// socket.on('gameclose', (id, status) => {
-	// 	console.log("Player " + id + " called gameclose()");
-	// 	let con = mysql.createConnection(connection_options);
-	// 	con.connect((err) => { if (err) throw err; });
-	// 	let sql;
-	// 	if(status == "won")
-	// 		sql = "update users set wins = wins + 1 where id = ?;";
-	// 	if(status == "lost")
-	// 		sql = "update users set losses = losses + 1 where id = ?;";
-	// 	con.query(sql, [id], (err, result) => {
-	// 		if (err) throw err;
-	// 		con.end();
-	// 	});	
-	// });
 });
 
 module.exports = io;
