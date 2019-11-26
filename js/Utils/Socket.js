d4ad86cce3bc9635c3d7166f17a9dd1fcf39ddc7
ddk16a <ddk16a@acu.edu>
2019-11-25 20:45:47 -0600

    think

--- js/Utils/Socket.js
+++ js/Utils/Socket.js
@@ -50,6 +50,9 @@ socket.on('paired', () => {
 });
 
 socket.on('updateBoard', (piece, dest) => {
+	let p = Stage.get(Stage.invert(dest.row), dest.col);
+	if (p)
+		Stage.remove(p);
 	Stage.get(Stage.invert(piece.row), piece.col).moveTo(Stage.invert(dest.row), dest.col);
 	Stage.startTurn();
 	Stage.update();
